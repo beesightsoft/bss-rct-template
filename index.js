c9ac843558b7b3cb13e03170f4aab12e2b45c774
Nhan Cao <nhancv92@gmail.com>
2018-02-01 12:36:56 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+global.NTron = (__DEV__ && console.tron) ? console.tron.log : () => {};
+global.NLog = (__DEV__) ? console.log : () => {};
+global.NLoge = (__DEV__) ? console.error : () => {};
+
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
