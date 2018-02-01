6154ba1ddcc87090567f01e4e5d8cad265fd4795
Nhan Cao <nhancv92@gmail.com>
2018-02-01 16:23:43 +0700

    update global log, avoid crash on release build

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-global.NTron = (__DEV__ && console.tron) ? console.tron.log : () => {};
-global.NLog = (__DEV__) ? console.log : () => {};
-global.NLoge = (__DEV__) ? console.error : () => {};
+__DEV__ ? global.NTron = console.tron.log : global.NTron = () => { }
+__DEV__ ? global.NLog = console.log : global.NLog = () => { }
+__DEV__ ? global.NLoge = console.error : global.NLoge = () => { }
 
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
