16da5e1752c372f291e8badec1f8db4a885dd447
Nhan Cao <nhancv92@gmail.com>
2019-02-25 22:46:12 +0700

    #2. upgrade version

--- App/Config/index.js
+++ App/Config/index.js
@@ -3,10 +3,13 @@ import DebugConfig from './DebugConfig'
 import AppConfig from './AppConfig'
 
 // Allow/disallow font-scaling in app
-Text.defaultProps.allowFontScaling = AppConfig.allowTextFontScaling
+// react native 0.57.8
+Text.allowFontScaling = AppConfig.allowTextFontScaling;
 
 if (__DEV__) {
   // If ReactNative's yellow box warnings are too much, it is possible to turn
   // it off, but the healthier approach is to fix the warnings.  =)
   console.disableYellowBox = !DebugConfig.yellowBox
+} else {
+  console.disableYellowBox = true
 }
